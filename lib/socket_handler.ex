72a572c954139a42e4c9836e42a181773854bd52
Mononaut <github@monospace.live>
2021-03-30 16:29:38 -0600

    Resend last block on request

--- lib/socket_handler.ex
+++ lib/socket_handler.ex
@@ -1,6 +1,8 @@
 defmodule BitcoinStream.SocketHandler do
   @behaviour :cowboy_websocket
 
+  alias BitcoinStream.Protocol.Block, as: BitcoinBlock
+
   def init(request, state) do
     {:cowboy_websocket, request, state}
   end
@@ -12,9 +14,44 @@ defmodule BitcoinStream.SocketHandler do
     {:ok, state}
   end
 
+  def load_block() do
+    case File.read("block.dat") do
+      {:ok, blockData } ->
+        case BitcoinBlock.decode(blockData) do
+          {:ok, block} ->
+            IO.puts('file decoded ok')
+            case Jason.encode(%{type: "block", block: block}) do
+              {:ok, payload} ->
+                IO.puts("block encoded ok");
+                { :ok, payload };
+              {:error, reason} ->
+                IO.puts("Error json encoding block: #{reason}");
+                :error
+            end
+          {:error, reason} ->
+            IO.puts("Block decoding failed: #{reason}");
+            :error
+        end
+
+      {:error, reason} ->
+        IO.puts("Reading block file failed: #{reason}");
+        :error
+    end
+  end
+
   def websocket_handle({:text, msg}, state) do
     IO.puts("message received: #{msg}");
-    {:reply, {:text, msg}, state}
+    case msg do
+      "hb" -> {:reply, {:text, msg}, state};
+      "block" ->
+        IO.puts('block request')
+        case load_block() do
+          {:ok, blockMsg} ->
+            {:reply, {:text, blockMsg}, state};
+
+          _ -> {:reply, {:text, 'error'}, state}
+        end
+    end
   end
 
   def websocket_info(info, state) do
