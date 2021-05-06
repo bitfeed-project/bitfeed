b14b4907e8a91d8fd4b26b5970268c338737b0ab
Mononaut <github@monospace.live>
2021-05-05 20:41:53 -0600

    Add input/output data to block txs

--- src/models/BitcoinTx.js
+++ src/models/BitcoinTx.js
@@ -2,23 +2,21 @@ import TxView from './TxView.js'
 import config from '../config.js'
 
 export default class BitcoinTx {
-  constructor ({ version, id, value, inputs, outputs, witnesses, time, block }, vertexArray) {
+  constructor ({ version, id, value, inputs, outputs, time, block }, vertexArray) {
     this.version = version
     this.id = id
     this.vertexArray = vertexArray
     this.pixelPosition = { x: 0, y: 0, r: 0}
     this.screenPosition = { x: 0, y: 0, r: 0}
     this.gridPosition = { x: 0, y: 0, r: 0}
+    this.inputs = inputs
+    this.outputs = outputs
+    this.value = value
 
-    if (inputs && outputs) {
-      this.inputs = inputs
-      this.outputs = outputs
+    if (inputs && outputs && value == null) {
       this.value = this.calcValue()
-    } else if (value) {
-      this.value = value
     }
 
-    this.witnesses = witnesses
     this.time = time
 
     if (config.donationHash && this.outputs) {
