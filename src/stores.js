dd13cef57cf9ed8da3b8e1b30cb056aa081954d2
Mononaut <github@monospace.live>
2021-07-30 11:51:56 -0600

    Disable about overlay for new visitors

--- src/stores.js
+++ src/stores.js
@@ -80,4 +80,5 @@ export const sidebarToggle = writable(null)
 export const nativeAntialias = writable(false)
 
 const newVisitor = !localStorage.getItem('seen-welcome-msg')
-export const overlay = writable(newVisitor ? 'about' : null)
+// export const overlay = writable(newVisitor ? 'about' : null)
+export const overlay = writable(null)
