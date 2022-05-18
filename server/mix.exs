3417e49d3f05a26710e31ff316cc426c262f4de0
Mononaut <github@monospace.live>
2022-05-18 14:40:37 +0000

    Update version to 2.3.1

--- server/mix.exs
+++ server/mix.exs
@@ -4,7 +4,7 @@ defmodule BitcoinStream.MixProject do
   def project do
     [
       app: :bitcoin_stream,
-      version: "2.3.0",
+      version: "2.3.1",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
